4a3bc49df99e70512c97c0070aeb965e6016b70e
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-08-31 16:54:40 -0300

    Criação de refeições - POST
    Criação básica para refeição. Sem validações além do zod ainda.

--- backend/src/routes/meals.ts
+++ backend/src/routes/meals.ts
@@ -1,12 +1,28 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
 import { knex } from '../database';
+import { randomUUID } from 'crypto';
 
 export async function mealsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    // { title, description, isInDiet }
 
-  app.get('/meals', async () => {
-    const meals = await knex('meals')
-      .select('*');
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      description: z.string(),
+      isInDiet: z.boolean(),
+    });
 
-    return meals;
+    const { title, description, isInDiet } = createTransactionBodySchema.parse(request.body);
+
+    await knex('meals')
+      .insert({
+        id: randomUUID(),
+        title,
+        description,
+        isInDiet,
+      });
+
+    return reply.status(201).send();
   });
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
 import fastify from 'fastify';
-import { knex } from './database';
 import { env } from './env';
 import { mealsRoutes } from './routes/meals';
 
 const app = fastify();
 
-app.register(mealsRoutes);
+app.register(mealsRoutes, {
+  prefix: 'meals',
+});
 
 app.listen({
   port: env.PORT,
